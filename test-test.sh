75393dfd1c2879de725b99025c0a43288e513088
ramesh <ramesh@nirvaha.com>
2017-01-17 11:41:56 -0800

    bug

--- test-test.sh
+++ test-test.sh
@@ -9,14 +9,14 @@ no="no"
 while read line
 do
         name=$line
-        if [[ $name == "SUCCESS" ]] ;  
+        if [ "$name" = "$searchString1" ] ;  
         then
 		echo succeeded
                 success="yes"
         fi
 done < run-tests-on-test.txt 
 
-if [[ $success == "$no" ]];
+if [[ "$success" = "$no" ]];
 then
 # rollback
 	echo rolling-back
@@ -24,7 +24,7 @@ then
 	sh deploy-test.sh
 	exit 1
 fi
-if [[ $success == "$yes" ]];
+if [[ "$success" = "$yes" ]];
 then
 # rollback
 	echo correcting-last-failed
